7606b7d8b1bf2820189b5c4f730ca0b2d7036572
Xia Yuang <xiayuang@foxmail.com>
2024-04-21 16:00:37 +0800

    Switch to yauzl instead of unzipper.

--- updateMeta.js
+++ updateMeta.js
@@ -1,8 +1,9 @@
 const fs = require('fs')
-const path = require('path')
+// const path = require('path')
 const xml2js = require('xml2js')
 const archiver = require('archiver')
-const unzipper = require('unzipper')
+// const unzipper = require('unzipper')
+const yauzl = require('yauzl')
 const { computeArchiveId } = require('./computeArchiveId')
 
 module.exports = {
@@ -37,12 +38,15 @@ module.exports = {
     const xmlObj = builder.buildObject(xmlData)
 
     const output = fs.createWriteStream(outputPath)
+
     const archive = archiver('zip', {
       zlib: { level: 0 } // No compression
     })
     archive.pipe(output)
     archive.append(Buffer.from(xmlObj), { name: 'ComicInfo.xml' })
 
+    /*
+    //use unzipper to read the archive
     const unzipStream = fs
       .createReadStream(archivePath)
       .pipe(unzipper.Parse({ forceStream: true }))
@@ -55,10 +59,45 @@ module.exports = {
         entry.autodrain()
       }
     }
+    */
+
+    // use yauzl to read the archive
+    yauzl.open(
+      archivePath,
+      { autoClose: false, lazyEntries: true },
+      function (err, zipfile) {
+        if (err) throw err
+        zipfile.readEntry()
+        zipfile.on('entry', function (entry) {
+          if (
+            /\/$/.test(entry.fileName) ||
+            /ComicInfo.xml/.test(entry.fileName)
+          ) {
+            // Directory file names end with '/'.
+            // Note that entries for directories themselves are optional.
+            // An entry's fileName implicitly requires its parent directories to exist.
+            zipfile.readEntry()
+          } else {
+            // file entry
+            zipfile.openReadStream(entry, function (err, readStream) {
+              if (err) throw err
+              archive.append(readStream, { name: entry.fileName })
+              readStream.on('end', function () {
+                zipfile.readEntry()
+              })
+            })
+          }
+        })
+        zipfile.once('end', function () {
+          zipfile.close()
+          archive.finalize()
+        })
+      }
+    )
+
     await new Promise((resolve, reject) => {
       output.on('close', () => resolve())
       archive.on('error', (err) => reject(err))
-      archive.finalize()
     })
 
     return { ...archiveInfo, arcid: computeArchiveId(outputPath) }
